120f86745632a685e0fc12cf6e1db343f0b250fd
Aaron Pennington <smokeyblues@hotmail.com>
2018-04-10 11:46:03 -0600

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>Hi, Welcome to Yakkee</h1>
+            </header>
+            <div className="content">
+                <p>A video chat service for seniors<br />
+                and residents of assisted living.</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
---
+++ src/pages/index.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import Banner from '../components/Banner'
+
+import pic01 from '../assets/images/pic01.jpg'
+import pic02 from '../assets/images/pic02.jpg'
+import pic03 from '../assets/images/pic03.jpg'
+import pic04 from '../assets/images/pic04.jpg'
+import pic05 from '../assets/images/pic05.jpg'
+import pic06 from '../assets/images/pic06.jpg'
+import happyCouple from '../assets/images/happy-couple.jpg'
+
+class HomeIndex extends React.Component {
+    render() {
+        const siteTitle = this.props.data.site.siteMetadata.title
+        const siteDescription = this.props.data.site.siteMetadata.description
+
+        return (
+            <div>
+                <Helmet>
+                    <title>{siteTitle}</title>
+                    <meta name="description" content={siteDescription} />
+                </Helmet>
+
+                <Banner />
+
+                <div id="main">
+                    <section id="one" className="tiles">
+                        <article style={{backgroundImage: `url(${pic01})`}}>
+                            <header className="major">
+                                <h3>You are here</h3>
+                                <p>And your friends and family are there.</p>
+                                <br></br>
+                                <p>Wouldn't it be great if you could visit them whenever you wanted to?</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic02})`}}>
+                            <header className="major">
+                                <h3>Yakkee exists to erase the distance</h3>
+                                <p>between friends and family that aren't able to actually get together as much as they would like.</p>
+                                <br></br>
+                                <p>Yakkee is designed to make it easy for people spend time together, even if they can't physically visit.</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${pic03})`}}>
+                            <header className="major">
+                                <h3>So easy to use</h3>
+                                <p>The Yakkee interface is designed to intuitive and easy to use. Paying special attention to the particular needs of our seniors and residents of assisted living, Yakkee is designed with adaptive technology built in to be there when you need it.</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                        <article style={{backgroundImage: `url(${happyCouple})`}}>
+                            <header className="major">
+                                <h3>Sign up Today</h3>
+                                <p>And bring your friends and family to you!</p>
+                            </header>
+                            <Link to="/landing" className="link primary"></Link>
+                        </article>
+                    </section>
+                    <section id="two">
+                        <div className="inner">
+                            <header className="major">
+                                <h2>Massa libero</h2>
+                            </header>
+                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                            <ul className="actions">
+                                <li><Link to="/landing" className="button next">Get Started</Link></li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default HomeIndex
+
+export const pageQuery = graphql`
+    query PageQuery {
+        site {
+            siteMetadata {
+                title
+                description
+            }
+        }
+    }
+`
